b8bb27ba8080413a7cedc697ecc53737145b7a00
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-12-12 08:48:03 +0900

    added go.mod

---
+++ p006/p006.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println(input)
+}
---
+++ welcome-to-atcoder/welcome-to-atcoder.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c, s string
+
+	fmt.Scanln(a)
+	fmt.Scanln(b)
+	fmt.Scanln(c)
+	fmt.Scanln(s)
+
+}
