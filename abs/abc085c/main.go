8ee910b260c857d6a55785394228830eaf543c10
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-03-28 10:46:55 +0900

    add : abcc

--- abs/abc085c/main.go
+++ abs/abc085c/main.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	// input
 	var N, Y int
 	fmt.Scan(&N, &Y)
 
+	for i := N; i >= 0; i-- {
+		for j := N - i; j >= 0; j-- {
+			k := N - i - j
+			if 10000*i+5000*j+1000*k == Y {
+				fmt.Println(i, j, k)
+				return
+			}
+		}
+	}
+	fmt.Println(-1, -1, -1)
 }
