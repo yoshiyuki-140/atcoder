e6a5da8619923be33bbd9822d9d449bf2f40fa8c
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-03-28 16:14:18 +0900

    add : Plan構造体に与えられたスライスの長さが1の時の処理を追加

--- abs/abc086c/main.go
+++ abs/abc086c/main.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 )
 
-func isTravelPlanFeasible(N int, schedule [][]int) string {
+type Plan struct {
+	t, x, y int
+}
+
+func isTravelPlanFeasible(N int, schedules []Plan) string {
+	if len(schedules) == 1 {
+		if schedules[0].t < schedules[0].x+schedules[0].y || (schedules[0].t-schedules[0].x-schedules[0].y)%2 != 0 {
+			return "No"
+		}
+		return "Yes"
+	}
 	for i := 0; i < N-1; i++ {
-		dt := schedule[i+1][0] - schedule[i][0]      // 時間の差分
-		dx := abs(schedule[i+1][1] - schedule[i][1]) // x座標の差分
-		dy := abs(schedule[i+1][2] - schedule[i][2]) // y座標の差分
+		dt := schedules[i+1].t - schedules[i].t      // 時間の差分
+		dx := abs(schedules[i+1].x - schedules[i].x) // x座標の差分
+		dy := abs(schedules[i+1].y - schedules[i].y) // y座標の差分
 
 		if dt < dx+dy || (dt-dx-dy)%2 != 0 { // 時間の差分が座標の差分より小さい、または時間と座標の偶奇が一致しない場合
 			return "No"
@@ -16,10 +26,9 @@ func isTravelPlanFeasible(N int, schedule [][]int) string {
 	}
 	return "Yes"
 }
-
 func abs(x int) int {
 	if x < 0 {
-		return -x
+		return -1 * x
 	}
 	return x
 }
@@ -28,12 +37,11 @@ func main() {
 	var N int
 	fmt.Scan(&N)
 
-	schedule := make([][]int, N)
+	schedules := make([]Plan, N)
 	for i := 0; i < N; i++ {
-		schedule[i] = make([]int, 3)
-		fmt.Scan(&schedule[i][0], &schedule[i][1], &schedule[i][2])
+		fmt.Scan(&schedules[i].t, &schedules[i].x, &schedules[i].y)
 	}
 
-	result := isTravelPlanFeasible(N, schedule)
+	result := isTravelPlanFeasible(N, schedules)
 	fmt.Println(result)
 }
