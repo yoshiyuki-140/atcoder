f974088f07c9c789b9fbdbe80df1825d21dd8355
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-04-29 22:42:18 +0900

    add : 解説を見てからc++で書かれたのをgoに置き換えてみたコードを追加

--- abc351/c/main.go
+++ abc351/c/main.go
@@ -2,36 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	// init
-	N := 0
-	fmt.Scan(&N)
-	ballSizes := make([]int, N)
-	for i := 0; i < N; i++ {
-		fmt.Scan(&ballSizes[i])
-		ballSizes[i] = int(math.Pow(2, float64(ballSizes[i])))
-	}
-
-	// main logic
-	column := make([]int, 0)
-	for _, ballSize := range ballSizes {
-		column = append(column, ballSize)
-		for {
-			if len(column) <= 1 {
-				break
-			}
-			last := column[len(column)-1]
-			penultimate := column[len(column)-2]
-			if last == penultimate {
-				column = column[:len(column)-2]
-				column = append(column, last+penultimate)
+	var n, l int
+	var a [20000]int
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[l])
+		l++
+		for l > 1 {
+			if a[l-2] == a[l-1] {
+				a[l-2]++
+				l--
 			} else {
 				break
 			}
 		}
 	}
-	fmt.Println(len(column))
+	fmt.Println(l)
 }
