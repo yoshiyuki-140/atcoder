94fc03534ae33b1ef86b1f3d96cc82aae21e5252
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-04 22:08:05 +0900

    add : topのみに注視した計算方法

--- abc352/c/main.go
+++ abc352/c/main.go
@@ -16,15 +16,11 @@ func main() {
 	for i := range Gs {
 		fmt.Scan(&Gs[i].A, &Gs[i].B)
 	}
-	P := make([]int, N)
-	for i := range P {
-		P[i] = i
-	}
 
 	// main logic
-	maxHeight := 0
-	for NextPermutation(P) {
-		height := getHeight(&Gs, P)
+	maxHeight, height := 0, 0
+	for i := range Gs {
+		height = getHeight(&Gs, i)
 		if height > maxHeight {
 			maxHeight = height
 		}
@@ -32,52 +28,15 @@ func main() {
 	fmt.Println(maxHeight)
 }
 
-func getHeight(gArray *[]Giant, P []int) int {
+func getHeight(gArray *[]Giant, top int) int {
 	// その順列の時の積み上げた巨人達の高さ
 	H := 0
-	for i, p := range P {
-		if i == len(P)-1 {
-			H += (*gArray)[p].B
-			break
+	for i, g := range *gArray {
+		if i == top {
+			H += g.B
+		} else {
+			H += g.A
 		}
-		H += (*gArray)[p].A
 	}
 	return H
-}
-
-func NextPermutation(a []int) bool {
-	// 与えられた順列の次の順列をみつける関数
-
-	// ピボットを見つける
-	last := len(a) - 2
-	for last >= 0 && a[last] >= a[last+1] {
-		last--
-	}
-	if last < 0 {
-		return false // これ以上新しい順列は存在しない
-	}
-
-	// ピボットより大きい最小の要素を見つける
-	next := len(a) - 1
-	for a[next] <= a[last] {
-		next--
-	}
-
-	// 要素を交換
-	a[last], a[next] = a[next], a[last]
-
-	// ピボットの直後から末尾までを反転
-	reverse(a, last+1, len(a)-1)
-
-	return true
-}
-
-// reverse はスライスの指定された範囲を反転させます。
-func reverse(a []int, start, end int) {
-	// スライスオブジェクトの範囲反転
-	for start < end {
-		a[start], a[end] = a[end], a[start]
-		start++
-		end--
-	}
 }
